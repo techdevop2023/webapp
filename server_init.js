3cf82bfa3c3fecdec3f91210d2dd1ca59416991c
techdevop2023 <143237981+techdevop2023@users.noreply.github.com>
2024-01-10 15:09:13 +0530

    Create server_init.js

---
+++ server_init.js
@@ -0,0 +1,13 @@
+// Load express module using `require` directive 
+let express = require('express') 
+let app = express() 
+  
+// Define request response in root URL (/) 
+app.get('/', function (req, res) { 
+  res.send('Dockerize the node app') 
+}) 
+  
+// Launch listening server on port 3000 
+app.listen(8081, function () { 
+  console.log('app listening on port 3000') 
+})
